5981d36f2acfa5d57927806666dd3ff0b1954aaa
Chino <Chino@DESKTOP-F2S7L7R>
2021-07-14 19:04:00 -0500

    tercer comit

--- ejercisios_asteristicos/src/ejercisios_asteristicos/asteriscos.java
+++ ejercisios_asteristicos/src/ejercisios_asteristicos/asteriscos.java
@@ -1,6 +1,22 @@
 package ejercisios_asteristicos;
 
+import java.util.Scanner;
+
 public class asteriscos {
 	// Segundo comit
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Introduce el numero de filas: ");
+		Scanner Leer = new Scanner(System.in);
+		int n = Leer.nextInt();
+		
+		for (int fila = 0; fila>= 0 ; fila --) {
+			for (int ast = 0; ast<= n ; ast ++ ) {
+				System.out.print("*");
+		}
+			System.out.println();	
+}
+	}
 }
