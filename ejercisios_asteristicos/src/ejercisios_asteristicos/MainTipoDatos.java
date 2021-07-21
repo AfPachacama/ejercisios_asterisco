06b5fb5ca8ab3aa88ebdc6321a00cfccc21c4860
Chino <Chino@DESKTOP-F2S7L7R>
2021-07-21 18:51:53 -0500

    Taller Datos

---
+++ ejercisios_asteristicos/src/ejercisios_asteristicos/MainTipoDatos.java
@@ -0,0 +1,19 @@
+package ejercisios_asteristicos;
+
+public class MainTipoDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short cantidadEstudiantes = 100;
+		byte entradaCircuito = 0;
+		int cantCiudadanos = 10000;
+		long cantidadMundo = 1000000;
+		float sueldo =  1200.56f;
+		double saldoBanco = 1000000.67;
+		char opcionMenu = “C”;
+		boolean casado = true;
+		
+
+	}
+
+}
