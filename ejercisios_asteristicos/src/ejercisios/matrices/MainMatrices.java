db22b36380eff37736608061cab3e22dd636578a
Chino <Chino@DESKTOP-F2S7L7R>
2021-08-02 20:04:30 -0500

    TallerClase2

---
+++ ejercisios_asteristicos/src/ejercisios/matrices/MainMatrices.java
@@ -0,0 +1,31 @@
+package ejercisios.matrices;
+
+public class MainMatrices {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1. Declarar una matriz A de tipo char con 5 filas y 4 columnas.
+		char A[][] = new char[5][4];
+		
+		//2. Declarar una matriz B de tipo char, con 5 filas y 6 columnas.
+		char B[][]= new char[5][6];
+		
+		//3. Declara una matriz miMatriz de long con 2 filas y 4 columnas.
+		long miMatriz[][]= new long[2][4];
+		
+		//4. Declara una matriz miMatriz1 de long con 1 fila y 2 columnas.
+		// y asignar a la posicion[0][0], el valor de 8, posicion[1][1] el valor de 8
+		//posicion[1][1] el valor de 8
+		//posicion[2][2] el valor de 2
+		long miMatriz1[][]= new long[1][2];
+		miMatriz[0][0] = 2;
+		miMatriz[1][1] = 8;
+		miMatriz[2][2] = 2;
+
+
+		
+		
+
+	}
+
+}
