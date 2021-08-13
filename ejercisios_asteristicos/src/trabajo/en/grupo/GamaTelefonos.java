1e3df988bd328e72a9cd3c2d1ee9f5a80f98e59b
Chino <Chino@DESKTOP-F2S7L7R>
2021-08-13 14:42:29 -0500

    TrabajoEnGrupo

---
+++ ejercisios_asteristicos/src/trabajo/en/grupo/GamaTelefonos.java
@@ -0,0 +1,24 @@
+//1. Declaracion del paquete
+package trabajo.en.grupo;
+
+public class GamaTelefonos {
+//2.Atributos
+
+	String Marca;
+	String Color;
+	String Modelo;
+	int imei;
+	String Camara;
+	String Almacenamiento;
+	String Ram;
+	String Procesador;
+
+	public void imprimir(String Marca, String Modelo, String Color, int imei, String Camara, String Almacenamiento,
+			String Ram, String Procesador) {
+		System.out.println("*******************************************************************************************************************");
+		System.out.println("IMPORTADORA POWERGAMA");
+		System.out.println("*******************************************************************************************************************");
+		System.out.println("La Gama De Celulares disponibles son: ");
+		System.out.println("Marca: " + Marca + "\nModelo: " + Modelo + "\nColor: " + Color+"\nImei: "+imei+"\nCamara: "+Camara+"\nAlmacenamiento: "+Almacenamiento+"\nRam: " + Ram +"\nProcesador: "+ Procesador);
+	}
+}
